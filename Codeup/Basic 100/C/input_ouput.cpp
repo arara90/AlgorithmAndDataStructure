6d451a18e41480e59c2bae52bc924e076a61ae28
ARA <ararajo@gmail.com>
2020-01-29 18:10:25 +0900

    codeup

---
+++ Codeup/Basic 100/C/input_ouput.cpp
@@ -0,0 +1,164 @@
+//// 1001 - 1027
+//
+//// 1001
+//#include <stdio.h>
+//int main()
+//{
+//  printf("Hello");
+//  return 0;
+//}
+//
+//// 1002
+//#include <stdio.h>
+//int main(){
+//    printf("Hello World");
+//    return 0;
+//    
+//}
+//
+//// 1003
+//#include <stdio.h>
+//int main(){
+//    printf("Hello\nWorld");
+//    return 0;
+//}
+//
+////1004
+//#include <stdio.h>
+//int main(){
+//    printf("\'Hello\'");
+//    return 0;
+//}
+//
+////1005
+//#include <stdio.h>
+//int main(){
+//    printf("\"Hello\"");
+//    return 0;
+//}
+//
+////1006
+//#include <stdio.h>
+//int main(){
+//    printf("\"!@#$%%^&*()\"");
+//    return 0;
+//}
+//
+////1007
+//#include <stdio.h>
+//int main(){
+//    printf("\"C:\\Download\\hello.cpp\"");
+//    return 0;
+//}
+//
+////1008
+//#include <stdio.h>
+//int main(){
+//    printf("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+//    return 0;
+//}
+//
+//
+////1010
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    scanf("%d", &n);
+//    printf("%d", n);
+//    return 0;
+//}
+
+//// 1011
+//#include <stdio.h>
+//int main(){
+//	char x;
+//	scanf("%c", &x);
+//	printf("%c", x);
+//	return 0;
+//}
+//
+//// 1012
+//#include <stdio.h>
+//int main(){
+//	float x;
+//	scanf("%f", &x);
+//	printf("%f", x);
+//	return 0;
+//}
+
+//// 1013
+//#include <stdio.h>
+//int main(){
+//	int a, b;
+//	scanf("%d%d", &a, &b);  //엔터/공백으로 입력 데이터가 구분되어 입력
+//	printf("%d %d", a, b);
+//}
+
+////1014
+//#include <stdio.h>
+//int main(){
+//	char a, b;
+//	scanf("%c %c", &a, &b);
+//	printf("%c %c", b, a);
+//	return 0;
+//}
+
+////1015
+//#include <stdio.h>
+//int main(){
+//	float a;
+//	scanf("%f", &a);
+//	printf("%.2f", a );
+//	return 0;
+//}
+
+
+////1017
+//#include <stdio.h>
+//int main(){
+//	int a;
+//	scanf("%d", &a);
+//	printf("%d %d %d", a,a,a);
+//	return 0;
+//}
+
+////1018
+//#include <stdio.h>
+//int main(){
+//	int h,m;
+//	scanf("%d:%d", &h, &m);
+//	printf("%d:%d", h, m);
+//	return 0;
+//}
+
+////1019
+//#include <stdio.h>
+//int main(){
+//	int y,m,d;
+//	scanf("%d.%d.%d", &y, &m, &d);
+//	printf("%04d.%02d.%02d", y, m, d);
+//	
+//	return 0;
+//}
+//
+
+
+/////★★★★★ 1020 000907-1121112
+//#include <stdio.h>
+//int main(){
+//	int a,b;
+//	scanf("%d-%d", &a, &b);
+//	printf("%06d%07d", a , b);
+//	return 0;
+//} 
+
+/////★★★★★ 1121 문자열 입력하기 
+//#include <stdio.h>
+//int main(){
+//    char data[51]="";
+//    scanf("%s", data);
+//    printf(data);
+//    return 0;
+//}
+
+
---
+++ Codeup/Basic 100/Python/In-output.py
@@ -0,0 +1,19 @@
+## 1018
+# 26
+# a = input().split(":")
+# print(f'{a[0]}:{a[1]}')
+
+#22 더 빠름
+# a = input().split(":")
+# print(a)
+# print("%s %s" % (a[0], a[1]))
+
+##1019
+# a = input().split(".")
+# yyyy, mm, dd = ("0000" + a[0], "0" + a[1],"0" + a[2])
+# print("%s.%s.%s" % (yyyy[-4:], mm[-2:], dd[-2:]))
+
+# a,b,c=input().split('.')
+# print('%04d' % int(a), end='.')
+# print('%02d' % int(b), end='.')
+# print('%02d' % int(c))
