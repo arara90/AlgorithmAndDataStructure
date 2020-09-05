4ce3dcac9b5edd4ef2955ef8905bdffbb1238441
arara90 <ararajo@gmail.com>
2020-09-05 16:00:37 +0900

    dp2

--- Programmers/practice/DP/commute.py
+++ Programmers/practice/DP/commute.py
@@ -66,19 +66,42 @@ print(solution(	4, 3, [[3, 1], [1, 3]]))
 #다른 사람 풀이
 # 훨씬 빠름 ㅜㅜ 
 
-def solution(m,n,puddles):
-    grid = [[0]*(m+1) for i in range(n+1)] #왼쪽, 위로 한줄씩 만들어서 IndexError 방지
-    if puddles != [[]]:                    #물이 잠긴 지역이 0일 수 있음
-        for a, b in puddles:
-            grid[b][a] = -1                #미리 -1로 체크
-    grid[1][1] = 1
-    for j in range(1,n+1):
-        for k in range(1,m+1):
-            if j == k == 1:                #(1,1)은 1로 만들어두고, 0이 되지 않도록
-                continue
-            if grid[j][k] == -1:           #웅덩이는 0으로 만들어 다음 덧셈 때 영향끼치지 않게
-                grid[j][k] = 0
-                continue
-            grid[j][k] = (grid[j][k-1] + grid[j-1][k])%1000000007   #[a,b] = [a-1,b] + [a,b-1] 공식
+# def solution(m,n,puddles):
+#     grid = [[0]*(m+1) for i in range(n+1)] #왼쪽, 위로 한줄씩 만들어서 IndexError 방지
+#     if puddles != [[]]:                    #물이 잠긴 지역이 0일 수 있음
+#         for a, b in puddles:
+#             grid[b][a] = -1                #미리 -1로 체크
+#     grid[1][1] = 1
+#     for j in range(1,n+1):
+#         for k in range(1,m+1):
+#             if j == k == 1:                #(1,1)은 1로 만들어두고, 0이 되지 않도록
+#                 continue
+#             if grid[j][k] == -1:           #웅덩이는 0으로 만들어 다음 덧셈 때 영향끼치지 않게
+#                 grid[j][k] = 0
+#                 continue
+#             grid[j][k] = (grid[j][k-1] + grid[j-1][k])%1000000007   #[a,b] = [a-1,b] + [a,b-1] 공식
 
-    return grid[n][m]
+#     return grid[n][m]
+
+# #     정확성  테스트
+# # 테스트 1 〉	통과 (0.01ms, 9.53MB)
+# # 테스트 2 〉	통과 (0.02ms, 9.38MB)
+# # 테스트 3 〉	통과 (0.03ms, 9.46MB)
+# # 테스트 4 〉	통과 (0.07ms, 9.5MB)
+# # 테스트 5 〉	통과 (0.08ms, 9.52MB)
+# # 테스트 6 〉	통과 (0.06ms, 9.39MB)
+# # 테스트 7 〉	통과 (0.06ms, 9.53MB)
+# # 테스트 8 〉	통과 (0.13ms, 9.46MB)
+# # 테스트 9 〉	통과 (0.06ms, 9.45MB)
+# # 테스트 10 〉	통과 (0.03ms, 9.57MB)
+# # 효율성  테스트
+# # 테스트 1 〉	통과 (3.37ms, 9.89MB)
+# # 테스트 2 〉	통과 (1.52ms, 9.45MB)
+# # 테스트 3 〉	통과 (1.72ms, 9.5MB)
+# # 테스트 4 〉	통과 (2.29ms, 9.75MB)
+# # 테스트 5 〉	통과 (2.29ms, 9.5MB)
+# # 테스트 6 〉	통과 (3.05ms, 9.7MB)
+# # 테스트 7 〉	통과 (1.68ms, 9.37MB)
+# # 테스트 8 〉	통과 (2.32ms, 9.67MB)
+# # 테스트 9 〉	통과 (2.47ms, 9.59MB)
+# # 테스트 10 〉	통과 (2.73ms, 9.63MB)
