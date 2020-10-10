623dcc41f59c5e5bf2c0167abcab53aa03f6bded
arara90 <ararajo@gmail.com>
2020-10-10 16:18:50 +0900

    dp example 1

--- Practice_python/DynamicProgramming/금광.py
+++ Practice_python/DynamicProgramming/금광.py
@@ -14,10 +14,60 @@
 # 출력 : 테스트 케이스마다 체굴자가 얻을 수 있는 금의 최대 크기 (테스트 케이스틑 줄바꿈으로 구분)
 
 
-def getRes(n=3, m=4, array=[1, 3, 3, 2, 2, 1, 4, 1, 0, 6, 4, 7]):
-    dp_list = []
 
-    return(dp_list)
+# def getRes(n=3, m=4, array=[
+#     1, 3, 3, 2, 
+#     2, 1, 4, 1,
+#     0, 6, 4, 7]
+#     ):
+
+#     num_map = [[]] * m
+#     # for i in range(m):
+#     #     num_map[i] = [ array[i+m*j] for j in range(n) ]
+    
+#     return(num_map)
 
 
-print(getRes())
+# print(getRes())
+# print(getRes(4,4,[1,3,1,5,2,2,4,1,5,0,2,3,0,6,1,2]))
+
+# 4 4
+# 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
+
+
+## 풀이
+# dp[i][j] = array[i][j] + max(dp[i-1][j-1] , dp[i][j-1], dp[i+1][j-1] )
+
+
+for tc in range(int(input())):
+    n,m = map(int, input().split())
+    array =list(map(int, input().split()))
+
+    #다이나믹 프로그래밍을 위한 2차원 DP테이블 초기화
+    dp = []
+    index = 0
+
+    for i in range(n):
+        dp.append(array[index:index+m])
+        index += m
+
+    #다이나믹 프로그래밍 진행
+    for j in range(1,m):
+        for i in range(n):
+            #왼쪽 위에서 오는 경우
+            if i==0: left_up = 0
+            else: left_up=dp[i-1][j-1]
+
+            #왼쪽 아래에서 오는 경우
+            if i==n-1: left_down = 0
+            else: left_down=dp[i+1][j-1]
+
+            #왼쪽에서 오는 경우
+            left = dp[i][j-1]
+            dp[i][j] = dp[i][j] + max(left, left_down, left_up)
+
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m-1])
+
+    print(result)
